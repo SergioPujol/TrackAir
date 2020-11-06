01739021432baf020ca691ce20b4b45ecdc6e960
SergioPujol <sergiopujmar3@gmail.com>
2020-11-06 03:10:26 +0100

    Pop up login fake

--- Android/AndroidStudio/app/src/main/java/com/example/serpumar/sprint0_3a/Fragments/PerfilFragment.java
+++ Android/AndroidStudio/app/src/main/java/com/example/serpumar/sprint0_3a/Fragments/PerfilFragment.java
@@ -1,16 +1,27 @@
 package com.example.serpumar.sprint0_3a.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.serpumar.sprint0_3a.LoginActivity;
 import com.example.serpumar.sprint0_3a.R;
 
 public class PerfilFragment extends Fragment {
+
+    public void setSesionIniciada(Boolean sesionIniciada) {
+        this.sesionIniciada = sesionIniciada;
+    }
+
+    private Boolean sesionIniciada = false;
 
 
 
@@ -21,7 +32,36 @@ public class PerfilFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //TODO si esta logeado, se cargan los datos, pero si no Boton login
+
+        View v = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        Button iniciarSesion = (Button) v.findViewById(R.id.boton_login);
+        TextView nombre = (TextView) v.findViewById(R.id.nombre_perfil);
+        TextView email = (TextView) v.findViewById(R.id.email_perfil);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        if(sesionIniciada) {
+            iniciarSesion.setVisibility(View.INVISIBLE);
+            nombre.setVisibility(View.VISIBLE);
+            email.setVisibility(View.VISIBLE);
+
+
+        } else {
+            iniciarSesion.setVisibility(View.VISIBLE);
+            nombre.setVisibility(View.INVISIBLE);
+            email.setVisibility(View.INVISIBLE);
+
+            iniciarSesion.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(getContext(), LoginActivity.class);
+                    startActivity(i);
+                }
+            });
+        }
+
+        return v;
     }
 }
---
+++ Android/AndroidStudio/app/src/main/java/com/example/serpumar/sprint0_3a/LoginActivity.java
@@ -0,0 +1,60 @@
+package com.example.serpumar.sprint0_3a;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends Activity {
+
+    private EditText nombre;
+
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_login);
+
+        nombre = (EditText) findViewById(R.id.input_nombre);
+
+        Button iniciarSesion = (Button) findViewById(R.id.btn_login);
+        iniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        popup();
+    }
+
+    private void popup() {
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*0.75), (int) (height*0.4));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = -25;
+
+        getWindow().setAttributes(params);
+    }
+
+    private void login() {
+
+        String nombreUser = nombre.getText().toString();
+
+    }
+
+
+}
